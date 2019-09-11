4f1d4f7104ee2eed4a4cff4d57e9aca6ac3a6961
jonathangiamp <jonathan.giamporcaro@gmail.com>
2019-09-11 23:20:22 +0200

    REFACTORING import safemath and secure arithmetic maths

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,8 +1,13 @@
 pragma solidity ^0.4.19;
 
 import "./ownable.sol";
+import "./safemath.sol";
 
 contract ZombieFactory is Ownable {
+
+    using SafeMath for uint256;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
 
     event NewZombie(uint zombieId, string name, uint dna);
 
@@ -28,7 +33,7 @@ contract ZombieFactory is Ownable {
         uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)) - 1;
         /* msg is a global variable */
         zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
+        ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
         NewZombie(id, _name, _dna);
     }
 
