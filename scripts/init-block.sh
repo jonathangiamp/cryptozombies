95047afc6e2877ef820be455664c2814466f7043
jonathangiamp <jonathan.giamporcaro@gmail.com>
2019-09-15 18:49:31 +0200

    CREATE some local script

---
+++ scripts/init-block.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# @dev Only run this command once to init your local/private Eth Blockchain
+geth --datadir blkchain init genesis.json
---
+++ scripts/run-js-console.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Connect to the local/private Ethereum blockchain when you blockchain is running
+geth attach http://127.0.0.1:8543
